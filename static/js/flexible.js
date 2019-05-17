a6be6db533e8d47baa9d4e41ed68acdc0e3d5bf6
饼干ink <bingganink@bingganinkdeMBP.lan>
2019-05-17 17:15:05 +0800

    字体mixin

--- static/js/flexible.js
+++ static/js/flexible.js
@@ -2,7 +2,7 @@
  * @Description: 移动端rem适配
  * @Author: 李佳馨
  * @Date: 2019-05-17 16:02:23
- * @LastEditTime: 2019-05-17 16:17:16
+ * @LastEditTime: 2019-05-17 17:10:07
  * @LastEditors: Please set LastEditors
  */
 
@@ -12,12 +12,18 @@
     // 获取dpr，设置meta标签
     var ua = navigator.userAgent
     var matches = ua.match(/Android[\S\s]+AppleWebkit\/(\d{3})/i)
+    var isAndroid = window.navigator.appVersion.match(/android/gi)
     var isIos = navigator.appVersion.match(/(iphone|ipad|ipod)/gi)
+    
     var dpr = window.devicePixelRatio || 1
-    if (!isIos && !(matches && matches[1] > 534)) {
+
+    if (isIos) {
+        dpr = (dpr >= 3 ? 3 : (dpr >= 2 ? 2 : 1))
+    } else if (isAndroid && !(matches && matches[1] > 534)) {
         // 如果非iOS, 非Android4.3以上, dpr设为1;
         dpr = 1
     }
+
     docEl.setAttribute('data-dpr', dpr)
     var scale = 1 / dpr
     var metaEl = document.querySelector('meta[name="viewport"]')
